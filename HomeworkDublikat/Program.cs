d2a8308bacbe13901c61f997d9ba3e4d508dd4f7
mike19952006 <mike19952006@gmail.com>
2018-03-05 15:30:32 +0200

    Удалить дубликаты из массива

--- HomeworkDublikat/Program.cs
+++ HomeworkDublikat/Program.cs
@@ -10,6 +10,22 @@ namespace HomeworkDublikat
     {
         static void Main(string[] args)
         {
+            List<int> listarray = new List<int>()
+            {
+                1,2,3,3,7,4,4,5,5
+            };
+            for (int i = 0; i < listarray.Count; i++)
+            {
+                for (int j = i+1; j < listarray.Count; j++)
+                {
+                    if (listarray[i] == listarray[j])
+                    {
+                        listarray.RemoveRange(j, 1);
+                    }
+                }
+                Console.WriteLine(listarray[i]);
+            }
+            
         }
     }
 }
